b18febb8520bf9bfa74c864e39f77051cc67a448
noaa <noaamaman325158@gmail.com>
2024-02-20 13:03:44 +0200

    More modular code.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import datetime
 
-# temporary gloval variable with the current location with the range
+# temporary global variable with the current location with the range
 chair_current_location = {
     'x': [-0.5, 0],
     'y': [1.8, 2.4]
@@ -29,8 +29,20 @@ def is_in_chair_surface(point, chair_loc):
     return point_in_range(chair_loc, point)
 
 
+def convert_current_log_file_data_to_dict(file_local_path):
+    log_file_data = {}
+    current_timestamp = None
+
+    with open(file_local_path, 'r') as file:
+        for line in file:
+            if line.startswith('timestamp'):
+                current_timestamp = line.split(': ')[1].strip()
+                log_file_data[current_timestamp] = {}
+            elif current_timestamp is not None:
+                key, value = line.strip().split('=')
+                log_file_data[current_timestamp][key] = float(value)
+    return log_file_data
+
+
 def calculate_total_time_in_chair_surface(log_data, chair_location):
     pass
-
-
-
